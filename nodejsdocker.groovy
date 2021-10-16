ede71c1d464a8c7013e51a54e0009225ac9fc6ea
lyes <lyes.boudia@gmail.com>
2021-10-16 17:41:00 +0100

    drp2

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -1,9 +1,9 @@
 
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/lbounif/deploy-example.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('Lynda Bounif')
-            node / gitConfigEmail('l_bounif@esi.dz')
+        git('https://github.com/lyes-boudia/deploy.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Lyes Boudia')
+            node / gitConfigEmail('lyes.boudia@gmail.com')
         }
     }
     triggers {
