f056f845dfacd512017b504cbe60eda8b511cb39
lyes <lyes.boudia@gmail.com>
2021-10-17 22:37:02 +0100

    change

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -10,12 +10,12 @@ job('NodeJS Docker example') {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('nodejs') // this is the name of the NodeJS installation
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('lbounif/docker-nodejs-demo')
+            repositoryName('lyes/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
